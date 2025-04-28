863ce5256b830456aca0e2e6123391bb1d9b7c66
mounikaran <mounikaran.mk@gmail.com>
2025-04-29 01:43:55 +0530

    fix: fronend env for api

--- app/services/api.js
+++ app/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL || "http://localhost:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export const sendSplitRequest = async (file, rowsPerFile, headerRows) => {
   const formData = new FormData();
